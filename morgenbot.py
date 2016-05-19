98d7a74d04bb3ba529229622fb4969a3b4fc377a
Bill Edgington <bedgington@triadretail.com>
2016-05-19 09:41:57 -0400

    Move idle user detection to when their turn comes

--- morgenbot.py
+++ morgenbot.py
@@ -154,7 +154,9 @@ def next(user):
         done()
     else:
         user = user[1:]
+        post_message('Current user list: @%s' % ', @'.join(users))
         if user:
+            post_message('Considering starting with @%s.' % user)
             if user not in active_users and user not in ignore_users and user not in absent_users:
                 post_message('I don\'t recognize that user.')
             elif user in ignore_users:
@@ -163,6 +165,8 @@ def next(user):
                 post_message('That user is absent.')
             elif user in active_users:
                 next_user_index = users.index(user)
+        else:
+            post_message('No user argument passed to next().')
 
         current_user = users.pop(next_user_index)
 
